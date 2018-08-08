4a347f1669554b05ec900f35a2330508b1804f69
User <User@PC-Lite>
2018-08-08 12:12:28 +0300

    hotfix 1

--- totalizator/src/main/java/by/epam/tote/command/LogoutCommand.java
+++ totalizator/src/main/java/by/epam/tote/command/LogoutCommand.java
@@ -1,12 +1,16 @@
 package by.epam.tote.command;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import by.epam.tote.constant.PageConstant;
 import by.epam.tote.constant.SessionConstant;
+import by.epam.tote.entity.FootballEvent;
 import by.epam.tote.router.RouteType;
 import by.epam.tote.router.Router;
+import by.epam.tote.service.ClientService;
 
 public class LogoutCommand implements ActionCommand {
 
@@ -22,6 +26,10 @@ public class LogoutCommand implements ActionCommand {
 		HttpSession session = request.getSession();
 		session.removeAttribute(SessionConstant.ROLE);
 		session.removeAttribute(SessionConstant.CLIENT);
+		session.invalidate();
+		ClientService service = new ClientService();
+		List<FootballEvent> events = service.findFootballEvents();
+		request.getSession().setAttribute(SessionConstant.FOOTBALL, events);
 		request.getSession().setAttribute(SessionConstant.MESSAGE, "message.logout_success");
 		return new Router(RouteType.FORWARD, PageConstant.MAIN_PAGE);
 	}
