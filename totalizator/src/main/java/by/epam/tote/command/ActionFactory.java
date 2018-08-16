8bd441b92cb050f47d211e2554096a568d56ff42
User <User@PC-Lite>
2018-08-16 16:30:51 +0300

    factory fix

--- totalizator/src/main/java/by/epam/tote/command/ActionFactory.java
+++ totalizator/src/main/java/by/epam/tote/command/ActionFactory.java
@@ -1,15 +1,10 @@
 package by.epam.tote.command;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 /**
  * A factory for creating Action objects.
  */
 public class ActionFactory {
 
-	/** The Constant LOGGER. */
-	private static final Logger LOGGER = LogManager.getLogger();
 
 	/**
 	 * Define command.
